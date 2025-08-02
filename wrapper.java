056dbee5c95934f7e0c39fcd17bf1c4e6c611532
Dhanyasri-Katta <2420030023@klh.edu.in>
2025-08-02 11:56:02 +0530

    Add files via upload

---
+++ wrapper.java
@@ -0,0 +1,14 @@
+package My_project;
+
+public class wrapper {
+	  public static void main(String args[]) {
+	    Integer a=2299;
+	    Boolean b=true;
+	    Double d=9.888d;
+	    int c=10;
+	    System.out.println(a.toString().length());
+	    System.out.println(d.toString().length());
+	    
+	  }
+
+}
